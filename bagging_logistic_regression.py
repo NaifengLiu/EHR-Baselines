4abd685cbd4115dcd541cdef71603bdc99868f2c
Godot <naifengliu.us@gmail.com>
2017-10-01 16:58:08 -0400

    add data

---
+++ bagging_logistic_regression.py
@@ -0,0 +1,72 @@
+import grouping
+import load_patient_info
+import numpy as np
+from tqdm import tqdm
+from sklearn.linear_model import LogisticRegression
+
+matching = grouping.matching
+missing = [1, 3, 8, 72, 165, 183, 223, 239, 285, 305, 324, 348, 376, 416, 446, 461, 486, 489, 548, 575, 599, 612, 632,
+           682, 683, 718, 721, 757, 777, 792, 799, 816, 819, 998, 1023, 1034, 1075, 1080, 1118, 1123, 1132, 1146, 1166,
+           1215]
+matching_keys = matching.keys()
+patients_info = load_patient_info.patients_info
+#######################################################################################################################
+x_train_file_names_positive = []
+x_test_file_names_positive = []
+
+for i in range(len(matching_keys)):
+    if i not in missing:
+        if len(x_train_file_names_positive) < 985:
+            x_train_file_names_positive.append(matching_keys[i])
+        else:
+            x_test_file_names_positive.append((matching_keys[i]))
+
+x_train_file_names_negative = []
+for i in range(200):
+    for item in x_train_file_names_positive:
+        x_train_file_names_negative.append(matching[item][i])
+x_test_file_names_negative = []
+for i in range(200):
+    for item in x_test_file_names_positive:
+        x_test_file_names_negative.append(matching[item][i])
+
+test = []
+for patient in x_test_file_names_positive:
+    test.append(patients_info[patient])
+for patient in x_test_file_names_positive:
+    for non_patient in matching[patient]:
+        test.append(patients_info[non_patient])
+test = np.array(test)
+
+#######################################################################################################################
+
+for fold_num in range(5):
+    print("start " + str(fold_num + 1) + " fold")
+    tmp_validation_names_positive = x_train_file_names_positive[fold_num * 197:(fold_num + 1) * 197]
+    tmp_training_names_positive = \
+        [item for item in x_train_file_names_positive if item not in tmp_validation_names_positive]
+
+    validation_X = []
+    for patient in tmp_validation_names_positive:
+        validation_X.append(patients_info[patient])
+    for patient in tmp_validation_names_positive:
+        for non_patient in matching[patient]:
+            validation_X.append(patients_info[non_patient])
+
+    this_fold_test_result = np.zeros(len(test))
+    this_fold_validation_result = np.zeros(197*201)
+
+    for j in tqdm(range(200)):
+        X = []
+        for item in tmp_training_names_positive:
+            X.append(patients_info[item])
+        for item in tmp_training_names_positive:
+            X.append(patients_info[matching[item][j]])
+        y = np.concatenate((np.zeros(788) + 1, np.zeros(788)), axis=0)
+        X = np.array(X)
+        logistic = LogisticRegression()
+        logistic.fit(X, y)
+        this_fold_test_result += logistic.predict_proba(test)[:, 1]
+        this_fold_validation_result += logistic.predict_proba(validation_X)[:, 1]
+    np.savetxt("./result/fold_" + str(fold_num+1) + "_test", this_fold_test_result)
+    np.savetxt("./result/fold_" + str(fold_num+1) + "_validation", this_fold_validation_result)
--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -1,25 +1,83 @@
 import numpy as np
 
-v_result = np.loadtxt("result/fold_1_validation").astype(int)
 
-v_rank = np.argsort(-v_result)
+def cal_validation_result_by_mean(num):
+    auc = np.zeros(10)
+    for i in range(num):
+        v_result = np.loadtxt("result/fold_"+str(i+1)+"_validation").astype(float)
+        v_rank = np.argsort(-v_result)
+        v_found = 0
+        count = 0
+        result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        # print v_rank
+        for j in v_rank:
+            if j <= 197:
+                v_found += 1
+            count += 1
+            ratio = v_found / float(197)
+            if 1 <= int(ratio/0.05) <= 10:
+                if result[int(ratio/0.05)-1] == 0:
+                    result[int(ratio/0.05)-1] = v_found / float(count) * 100
+        result = np.array(result)
+        # print result
+        auc += result
+    return np.true_divide(auc, num)
 
-print v_result[v_rank[0]]
-print v_rank[0]
 
-for i in v_result:
-    print i
+def cal_validation_result_by_combine(num):
+    auc = np.zeros(10)
+    v_result_1 = np.array([])
+    v_result_2 = np.array([])
+    for i in range(num):
+        tmp_v_result = np.loadtxt("result/fold_"+str(i+1)+"_validation").astype(float)
+        tmp_v_result_1 = tmp_v_result[:197]
+        tmp_v_result_2 = tmp_v_result[197:]
 
-# v_rank_final = v_rank
-# v_found = 0
-# count = 0
-# result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-# for i in v_rank_final:
-#     if i <= 197:
-#         v_found += 1
-#     count += 1
-#     ratio = v_found / float(197)
-#     if 1 <= int(ratio/0.05) <= 10:
-#         if result[int(ratio/0.05)-1] == 0:
-#             result[int(ratio/0.05)-1] = v_found / float(count) * 100
-# print result
+        v_result_1 = np.concatenate((v_result_1, tmp_v_result_1))
+        v_result_2 = np.concatenate((v_result_2, tmp_v_result_2))
+
+    v_result = np.concatenate((v_result_1, v_result_2))
+    v_rank = np.argsort(-v_result)
+    v_found = 0
+    count = 0
+    result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # print v_rank
+    for j in v_rank:
+        if j <= 985:
+            v_found += 1
+        count += 1
+        ratio = v_found / float(985)
+        if 1 <= int(ratio / 0.05) <= 10:
+            if result[int(ratio / 0.05) - 1] == 0:
+                result[int(ratio / 0.05) - 1] = v_found / float(count) * 100
+    result = np.array(result)
+    return result
+
+
+def cal_test_result(num):
+    auc = np.zeros(10)
+    v_result = np.zeros(41004)
+    for i in range(num):
+        v_result += np.loadtxt("result/fold_"+str(i+1)+"_test").astype(float)
+    v_rank = np.argsort(-v_result)
+    v_found = 0
+    count = 0
+    result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # print v_rank
+    for j in v_rank:
+        if j <= 204:
+            v_found += 1
+        count += 1
+        ratio = v_found / float(204)
+        if 1 <= int(ratio/0.05) <= 10:
+            if result[int(ratio/0.05)-1] == 0:
+                result[int(ratio/0.05)-1] = v_found / float(count) * 100
+    result = np.array(result)
+    return result
+
+
+print cal_validation_result_by_mean(5)
+
+print cal_validation_result_by_combine(5)
+
+print cal_test_result(5)
