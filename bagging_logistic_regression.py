df8c7481012dead7639f420f041c1eda375a39b5
Godot <naifengliu.us@gmail.com>
2017-10-01 21:08:23 -0400

    add data

--- bagging_logistic_regression.py
+++ bagging_logistic_regression.py
@@ -5,9 +5,6 @@ from tqdm import tqdm
 from sklearn.linear_model import LogisticRegression
 
 matching = grouping.matching
-missing = [1, 3, 8, 72, 165, 183, 223, 239, 285, 305, 324, 348, 376, 416, 446, 461, 486, 489, 548, 575, 599, 612, 632,
-           682, 683, 718, 721, 757, 777, 792, 799, 816, 819, 998, 1023, 1034, 1075, 1080, 1118, 1123, 1132, 1146, 1166,
-           1215]
 matching_keys = matching.keys()
 patients_info = load_patient_info.patients_info
 #######################################################################################################################
@@ -15,11 +12,10 @@ x_train_file_names_positive = []
 x_test_file_names_positive = []
 
 for i in range(len(matching_keys)):
-    if i not in missing:
-        if len(x_train_file_names_positive) < 985:
-            x_train_file_names_positive.append(matching_keys[i])
-        else:
-            x_test_file_names_positive.append((matching_keys[i]))
+    if len(x_train_file_names_positive) < 985:
+        x_train_file_names_positive.append(matching_keys[i])
+    else:
+        x_test_file_names_positive.append((matching_keys[i]))
 
 x_train_file_names_negative = []
 for i in range(200):
@@ -68,5 +64,5 @@ for fold_num in range(5):
         logistic.fit(X, y)
         this_fold_test_result += logistic.predict_proba(test)[:, 1]
         this_fold_validation_result += logistic.predict_proba(validation_X)[:, 1]
-    np.savetxt("./result/fold_" + str(fold_num+1) + "_test", this_fold_test_result)
-    np.savetxt("./result/fold_" + str(fold_num+1) + "_validation", this_fold_validation_result)
+    np.savetxt("./result/bagging_logistic_regression/fold_" + str(fold_num+1) + "_test", this_fold_test_result)
+    np.savetxt("./result/bagging_logistic_regression/fold_" + str(fold_num+1) + "_validation", this_fold_validation_result)
