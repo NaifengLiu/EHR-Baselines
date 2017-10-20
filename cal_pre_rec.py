5a8cb2d31ebd9cb8d995145bebbf0ef456f14653
Godot <naifengliu.us@gmail.com>
2017-10-20 10:05:59 -0400

    add method

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -130,22 +130,22 @@ def cal_pr(method):
 # print cal_validation_result_by_mean("logistic_regression", 197)
 # print " "
 #
-print "logistic regression result over validation set with method 2"
-print cal_validation_result_by_combine("logistic_regression", 197)
-print " "
-
-print "logistic regression result over test set"
-print cal_test_result("logistic_regression", 248)
-print " "
-#
-# print "logistic regression result over validation set with method"
-# print cal_validation_result_by_combine("logistic_regression_lasso", 197)
+# print "logistic regression result over validation set with method 2"
+# print cal_validation_result_by_combine("logistic_regression", 197)
 # print " "
 #
 # print "logistic regression result over test set"
-# print cal_test_result("logistic_regression_lasso", 248)
+# print cal_test_result("logistic_regression", 248)
 # print " "
-# #
+#
+print "logistic regression result over validation set with method"
+print cal_validation_result_by_combine("logistic_regression_lasso", 197)
+print " "
+
+print "logistic regression result over test set"
+print cal_test_result("logistic_regression_lasso", 248)
+print " "
+#
 # print "random forest result over validation set with method 1"
 # print cal_validation_result_by_mean("random_forest", 197)
 # print " "
