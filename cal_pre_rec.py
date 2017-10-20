25b73d3b73168ae528f5281b2c1464a140a7c6b1
Godot <naifengliu.us@gmail.com>
2017-10-20 07:19:05 -0400

    add method

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -102,6 +102,7 @@ def cal_pr(method):
 
     v_score = np.concatenate((v_result_1, v_result_2))
     y_test = np.concatenate((np.zeros(788) + 1, np.zeros(788*200)), axis=0)
+    print v_score[0:20]
     precision, recall, _ = precision_recall_curve(y_test, np.true_divide(v_score, 200))
     print precision, recall
 
