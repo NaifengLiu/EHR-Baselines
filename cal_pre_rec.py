40aa72244257c45c18c0cf5772dcaa6f039dae40
Godot <naifengliu.us@gmail.com>
2017-10-19 22:04:02 -0400

    add method

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -92,23 +92,31 @@ def cal_test_result(method, size):
 # print cal_validation_result_by_mean("logistic_regression", 197)
 # print " "
 #
-print "logistic regression result over validation set with method 2"
-print cal_validation_result_by_combine("logistic_regression", 197)
-print " "
-
-print "logistic regression result over test set"
-print cal_test_result("logistic_regression", 248)
-print " "
+# print "logistic regression result over validation set with method 2"
+# print cal_validation_result_by_combine("logistic_regression", 197)
+# print " "
 #
-print "random forest result over validation set with method 1"
-print cal_validation_result_by_mean("random_forest", 197)
-print " "
+# print "logistic regression result over test set"
+# print cal_test_result("logistic_regression", 248)
+# print " "
 #
-print "random forest result over validation set with method 2"
-print cal_validation_result_by_combine("random_forest", 197)
-print " "
+# print "logistic regression result over validation set with method"
+# print cal_validation_result_by_combine("logistic_regression_lasso", 197)
+# print " "
 #
-print "random forest result over test set"
-print cal_test_result("random_forest", 248)
-
-print cal_auc("logistic_regression")
+# print "logistic regression result over test set"
+# print cal_test_result("logistic_regression_lasso", 248)
+# print " "
+# #
+# print "random forest result over validation set with method 1"
+# print cal_validation_result_by_mean("random_forest", 197)
+# print " "
+# #
+# print "random forest result over validation set with method 2"
+# print cal_validation_result_by_combine("random_forest", 197)
+# print " "
+# #
+# print "random forest result over test set"
+# print cal_test_result("random_forest", 248)
+#
+# print cal_auc("logistic_regression")
