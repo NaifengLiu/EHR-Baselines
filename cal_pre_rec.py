753c95e8155dc85d7e63ac35ab9ead4e6418f6b8
Godot <naifengliu.us@gmail.com>
2017-10-20 08:52:58 -0400

    add method

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -2,6 +2,7 @@ import numpy as np
 from sklearn import metrics
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
+import matplotlib.pyplot as plt
 
 
 
@@ -109,24 +110,32 @@ def cal_pr(method):
     print v_score.shape
     y_test = np.concatenate((np.zeros(985) + 1, np.zeros(985*200)), axis=0)
     precision, recall, thresholds = precision_recall_curve(y_test, np.true_divide(v_score, 200))
-    print np.true_divide(v_score, 200)[0:20]
-    print(precision_score(y_test, np.true_divide(v_score, 200), average="macro"))
+    plt.step(recall, precision, color='b', alpha=0.2,
+             where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2,
+                     color='b')
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.savefig("tmp")
 
 
-# cal_pr("logistic_regression_lasso")
+cal_pr("logistic_regression_lasso")
 
 
 # print "logistic regression result over validation set with method 1"
 # print cal_validation_result_by_mean("logistic_regression", 197)
 # print " "
 #
-print "logistic regression result over validation set with method 2"
-print cal_validation_result_by_combine("logistic_regression", 197)
-print " "
-
-print "logistic regression result over test set"
-print cal_test_result("logistic_regression", 248)
-print " "
+# print "logistic regression result over validation set with method 2"
+# print cal_validation_result_by_combine("logistic_regression", 197)
+# print " "
+#
+# print "logistic regression result over test set"
+# print cal_test_result("logistic_regression", 248)
+# print " "
 #
 # print "logistic regression result over validation set with method"
 # print cal_validation_result_by_combine("logistic_regression_lasso", 197)
