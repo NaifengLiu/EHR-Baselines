610f1deacc86ace775b50d7becf45f0f4b38457d
Godot <naifengliu.us@gmail.com>
2017-10-20 12:54:40 -0400

    add method

--- general_random_forest.py
+++ general_random_forest.py
@@ -3,6 +3,7 @@ import load_patient_info
 import numpy as np
 from tqdm import tqdm
 from sklearn.ensemble import RandomForestClassifier
+import random
 
 matching = grouping.matching
 matching_keys = matching.keys()
@@ -26,6 +27,12 @@ for patient in x_test_file_names_positive:
 test = np.array(test)
 
 #######################################################################################################################
+negative_list = []
+for item in x_train_file_names_positive:
+    for one in matching_keys[item]:
+        negative_list.append(one)
+
+random.shuffle(negative_list)
 
 for fold_num in range(5):
     print("start " + str(fold_num + 1) + " fold")
@@ -41,19 +48,19 @@ for fold_num in range(5):
             validation_X.append(patients_info[non_patient])
 
     this_fold_test_result = np.zeros(len(test))
-    this_fold_validation_result = np.zeros(197*201)
+    this_fold_validation_result = np.zeros(197 * 201)
 
     for j in tqdm(range(200)):
         X = []
         for item in tmp_training_names_positive:
             X.append(patients_info[item])
-        for item in tmp_training_names_positive:
-            X.append(patients_info[matching[item][j]])
+        for item in negative_list[985*fold_num+j*788:985*fold_num+(j+1)*788]:
+            X.append(patients_info[item])
         y = np.concatenate((np.zeros(788) + 1, np.zeros(788)), axis=0)
         X = np.array(X)
         clf = RandomForestClassifier(max_depth=2, random_state=0)
         clf.fit(X, y)
         this_fold_test_result += clf.predict_proba(test)[:, 1]
         this_fold_validation_result += clf.predict_proba(validation_X)[:, 1]
-    np.savetxt("./result/bagging_random_forest/fold_" + str(fold_num+1) + "_test", this_fold_test_result)
-    np.savetxt("./result/bagging_random_forest/fold_" + str(fold_num+1) + "_validation", this_fold_validation_result)
+    np.savetxt("./result/random_forest/fold_" + str(fold_num + 1) + "_test", this_fold_test_result)
+    np.savetxt("./result/random_forest/fold_" + str(fold_num + 1) + "_validation", this_fold_validation_result)
