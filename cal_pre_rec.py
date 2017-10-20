349f83cbed793db1390e4cfeb0bb2d505b5c2294
Godot <naifengliu.us@gmail.com>
2017-10-20 08:36:58 -0400

    add method

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -52,7 +52,9 @@ def cal_validation_result_by_combine(method, size):
 
     v_result = np.concatenate((v_result_1, v_result_2))
     print v_result.shape
-    v_rank = np.argsort(v_result)
+    v_rank = np.argsort(-v_result)
+    for item in v_rank:
+        print item
     v_found = 0
     count = 0
     result = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
