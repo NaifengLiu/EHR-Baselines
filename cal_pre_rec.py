bcea1d446c5f956d665ab17dd2aa638128e684f4
Godot <naifengliu.us@gmail.com>
2017-10-27 13:31:54 -0400

    add method

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -166,6 +166,6 @@ def cal_pr(method):
 # print "logistic_regression result over test set"
 # print cal_test_result("logistic_regression", 248)
 #
-print cal_validation_result_by_combine("random_forest_shuffle_200", 40)
+print cal_validation_result_by_combine("random_forest_shuffle_100", 20)
 print " "
-print cal_test_result("random_forest_shuffle_200", 1033)
+print cal_test_result("random_forest_shuffle_100", 1133)
