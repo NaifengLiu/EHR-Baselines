f114407720d269abb7c40dff87ab5e65b27a49a1
Godot <naifengliu.us@gmail.com>
2017-11-15 09:26:07 -0500

    add method

---
+++ data/remove_er_proc.py
@@ -0,0 +1,23 @@
+with open("./raw_data/hae_pats.csv") as f:
+    title = f.readline().split(",")
+    print len(title)
+
+title.pop(0)
+del title[-1]
+title.remove("IDX_DT")
+title.remove("LOOKBACK_DT")
+
+print len(title)
+
+title_index = []
+
+for i in range(len(title)):
+    if len(title[i].split("ER_")) != 2:
+        if len(title[i].split("PRC_")) != 2:
+            title_index.append(i)
+            print title[i]
+
+print title_index
+print len(title_index)
+
+
