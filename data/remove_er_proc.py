fd871563151e9eb9d94631c0f0b53fd1c00d83c6
Godot <naifengliu.us@gmail.com>
2017-11-22 09:14:14 -0500

    add method

--- data/remove_er_proc.py
+++ data/remove_er_proc.py
@@ -15,11 +15,8 @@ for i in range(len(title)):
     if len(title[i].split("ER_")) != 2:
         if len(title[i].split("PRC_")) != 2:
             if len(title[i].split("lookback_")) != 2:
-                if len(title[i].split("RX_")) != 2:
-                    title_index.append(i)
-                    # print title[i]
-    if i-1==27:
-        print title[i]
+                title_index.append(i)
+                # print title[i]
 
 print title_index
 print len(title_index)
