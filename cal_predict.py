35999a679ddb5a4c41d0bf2f0662f443c47df4af
Godot <naifengliu.us@gmail.com>
2017-12-17 09:05:41 -0500

    add method

---
+++ cal_predict.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+size = 197
+
+tp = 0
+tn = 0
+fp = 0
+fn = 0
+
+auc = np.zeros(20)
+for i in range(5):
+    v_result = np.loadtxt("result/bagging_random_forest/fold_" + str(i + 1) + "_validation_predict").astype(float)
+    for j in range(len(v_result)):
+        if v_result[j] / float(200) >= 0.5 and j <= size:
+            tp += 1
+        elif v_result[j] / float(200) >= 0.5 and j > size:
+            fp += 1
+        elif v_result[j] / float(200) < 0.5 and j <= size:
+            fn += 1
+        elif v_result[j] / float(200) < 0.5 and j > size:
+            tn += 1
+    print tp, tn, fp, fn
