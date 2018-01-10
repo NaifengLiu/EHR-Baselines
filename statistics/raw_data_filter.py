c765835839d92ab90c35736b00c4adcc029a8844
Godot <naifengliu.us@gmail.com>
2018-01-09 21:06:26 -0500

    add method

--- statistics/raw_data_filter.py
+++ statistics/raw_data_filter.py
@@ -10,11 +10,11 @@ with open("nonhae_result") as ff:
 
 events = list(set(events))
 
-# with open("data/hae_all_1.csv") as f:
-#     with open("data/new_hae.csv", "w+") as w:
-#         for line in f:
-#             if line.split(",")[1] in events:
-#                 w.write(line)
+with open("data/hae_all_1.csv") as f:
+    with open("data/new_hae.csv", "w+") as w:
+        for line in f:
+            if line.split(",")[1] in events:
+                w.write(line)
 
 with open("data/nonhae.csv") as f:
     with open("data/new_non_hae.csv", "w+") as w:
