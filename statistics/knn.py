2c8a3a4c2234f9d6828e4a8372918c2cdc05f791
Godot <naifengliu.us@gmail.com>
2018-01-10 21:28:30 -0500

    add method

--- statistics/knn.py
+++ statistics/knn.py
@@ -3,59 +3,74 @@ import numpy as np
 import os
 from datetime import datetime
 
-print "loading folder"
 
-hae_folder_path = "./data/person/hae"
-non_hae_folder_path = "./data/person/nonhae"
+def run(a, b):
+    print "loading folder"
 
-hae_file_names = os.listdir(hae_folder_path)
-hae_file_names_length = len(hae_file_names)
+    hae_folder_path = "./data/person/hae"
+    non_hae_folder_path = "./data/person/nonhae"
 
-non_hae_file_names = os.listdir(non_hae_folder_path)[0:1233*50]
-non_hae_file_names_length = len(non_hae_file_names)
+    hae_file_names = os.listdir(hae_folder_path)
+    hae_file_names_length = len(hae_file_names)
 
-print "start generating matrix"
-print str(datetime.now())
+    non_hae_file_names = os.listdir(non_hae_folder_path)[0:1233*a]
+    non_hae_file_names_length = len(non_hae_file_names)
 
-X = []
-y = []
+    print "start generating matrix"
+    print str(datetime.now())
 
-print "entering hae patients"
-print str(datetime.now())
+    X = []
+    y = []
 
-for item in hae_file_names[0:int(hae_file_names_length*0.8)]:
-    X.append(np.loadtxt(hae_folder_path+"/"+item))
-    y.append(1)
+    print "entering hae patients"
+    print str(datetime.now())
 
-print "entering non hae patients"
-print str(datetime.now())
+    for item in hae_file_names[0:int(hae_file_names_length*0.8)]:
+        X.append(np.loadtxt(hae_folder_path+"/"+item))
+        y.append(1)
 
-for item in non_hae_file_names[0:int(non_hae_file_names_length*0.8)]:
-    X.append(np.loadtxt(non_hae_folder_path+"/"+item))
-    y.append(0)
+    print "entering non hae patients"
+    print str(datetime.now())
 
-print "start training"
-print str(datetime.now())
+    for item in non_hae_file_names[0:int(non_hae_file_names_length*0.8)]:
+        X.append(np.loadtxt(non_hae_folder_path+"/"+item))
+        y.append(0)
 
-neigh = KNeighborsClassifier(n_neighbors=1)
-neigh.fit(X, y)
+    print "start training"
+    print str(datetime.now())
 
-print "preparing testing data"
-print str(datetime.now())
+    neigh = KNeighborsClassifier(n_neighbors=b)
+    neigh.fit(X, y)
 
-test_X = []
+    print "preparing testing data"
+    print str(datetime.now())
 
-for item in hae_file_names[int(hae_file_names_length*0.8):hae_file_names_length]:
-    test_X.append(np.loadtxt(hae_folder_path+"/"+item))
-for item in non_hae_file_names[int(non_hae_file_names_length*0.8):non_hae_file_names_length]:
-    test_X.append(np.loadtxt(non_hae_folder_path+"/"+item))
+    test_X = []
 
-print "predicting"
-print str(datetime.now())
+    for item in hae_file_names[int(hae_file_names_length*0.8):hae_file_names_length]:
+        test_X.append(np.loadtxt(hae_folder_path+"/"+item))
+    for item in non_hae_file_names[int(non_hae_file_names_length*0.8):non_hae_file_names_length]:
+        test_X.append(np.loadtxt(non_hae_folder_path+"/"+item))
 
-test_y = neigh.predict(test_X)
+    print "predicting"
+    print str(datetime.now())
 
-print "saving results"
-print str(datetime.now())
+    test_y = neigh.predict(test_X)
 
-np.savetxt("501", test_y)
+    print "saving results"
+    print str(datetime.now())
+
+    np.savetxt(str(a) + str(b), test_y)
+
+
+run(20, 2)
+run(20, 4)
+run(20, 5)
+run(50, 2)
+run(50, 4)
+run(50, 5)
+run(100, 1)
+run(100, 2)
+run(100, 3)
+run(100, 4)
+run(100, 5)
