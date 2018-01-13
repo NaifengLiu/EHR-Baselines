bca730f0c6d60f1e62343e3da166fcd8444cca47
Godot <naifengliu.us@gmail.com>
2018-01-12 21:57:21 -0500

    add method

--- statistics/knn.py
+++ statistics/knn.py
@@ -39,7 +39,7 @@ def run(a, b):
     print "start training"
     print str(datetime.now())
 
-    neigh = KNeighborsClassifier(n_neighbors=b)
+    neigh = KNeighborsClassifier(n_neighbors=b, algorithm='ball_tree')
     neigh.fit(X, y)
 
     print "preparing testing data"
