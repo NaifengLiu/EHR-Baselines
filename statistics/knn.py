5f0e234efa9ad4d8a92db79c918586e955ef329d
Godot <naifengliu.us@gmail.com>
2018-01-18 21:39:38 -0500

    euclidean

--- statistics/knn.py
+++ statistics/knn.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 
-def run(a, b):
+def run(a, b, metric):
     print "loading folder"
 
     hae_folder_path = "./data/person/hae"
@@ -13,7 +13,7 @@ def run(a, b):
     hae_file_names = os.listdir(hae_folder_path)
     hae_file_names_length = len(hae_file_names)
 
-    non_hae_file_names = os.listdir(non_hae_folder_path)[0:1233*a]
+    non_hae_file_names = os.listdir(non_hae_folder_path)[0:1233 * a]
     non_hae_file_names_length = len(non_hae_file_names)
 
     print "start generating matrix"
@@ -25,21 +25,21 @@ def run(a, b):
     print "entering hae patients"
     print str(datetime.now())
 
-    for item in hae_file_names[0:int(hae_file_names_length*0.8)]:
-        X.append(np.loadtxt(hae_folder_path+"/"+item))
+    for item in hae_file_names[0:int(hae_file_names_length * 0.8)]:
+        X.append(np.loadtxt(hae_folder_path + "/" + item))
         y.append(1)
 
     print "entering non hae patients"
     print str(datetime.now())
 
-    for item in non_hae_file_names[0:int(non_hae_file_names_length*0.8)]:
-        X.append(np.loadtxt(non_hae_folder_path+"/"+item))
+    for item in non_hae_file_names[0:int(non_hae_file_names_length * 0.8)]:
+        X.append(np.loadtxt(non_hae_folder_path + "/" + item))
         y.append(0)
 
     print "start training"
     print str(datetime.now())
 
-    neigh = KNeighborsClassifier(n_neighbors=b, algorithm='auto', metric='l1')
+    neigh = KNeighborsClassifier(n_neighbors=b, algorithm='auto', metric=metric)
     neigh.fit(X, y)
 
     print "preparing testing data"
@@ -47,10 +47,10 @@ def run(a, b):
 
     test_X = []
 
-    for item in hae_file_names[int(hae_file_names_length*0.8):hae_file_names_length]:
-        test_X.append(np.loadtxt(hae_folder_path+"/"+item))
-    for item in non_hae_file_names[int(non_hae_file_names_length*0.8):non_hae_file_names_length]:
-        test_X.append(np.loadtxt(non_hae_folder_path+"/"+item))
+    for item in hae_file_names[int(hae_file_names_length * 0.8):hae_file_names_length]:
+        test_X.append(np.loadtxt(hae_folder_path + "/" + item))
+    for item in non_hae_file_names[int(non_hae_file_names_length * 0.8):non_hae_file_names_length]:
+        test_X.append(np.loadtxt(non_hae_folder_path + "/" + item))
 
     print "predicting"
     print str(datetime.now())
@@ -63,8 +63,8 @@ def run(a, b):
     np.savetxt(str(a) + str(b) + "l1", test_y)
 
 
-run(50, 1)
-run(50, 2)
-run(50, 3)
-run(50, 4)
-run(50, 5)
+run(50, 1, 'euclidean')
+run(50, 2, 'euclidean')
+run(50, 3, 'euclidean')
+run(50, 4, 'euclidean')
+run(50, 5, 'euclidean')
