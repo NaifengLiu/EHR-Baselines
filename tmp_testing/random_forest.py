6586f3282bb0d228a1fadc53fd9265fed42acb60
Godot <naifengliu.us@gmail.com>
2018-04-04 01:36:44 -0400

    change location

--- tmp_testing/random_forest.py
+++ tmp_testing/random_forest.py
@@ -9,6 +9,10 @@ y = np.loadtxt("y")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+X_train = np.delete(X_train, 0, axis=1)
+x_test_id = X_test[:, 0]
+X_test = np.delete(X_test, 0, axis=1)
+
 clf = RandomForestClassifier(n_estimators=10, max_depth=5, oob_score=True)
 
 clf.fit(X_train, y_train)
@@ -18,6 +22,14 @@ prob = clf.predict_proba(X_test)
 np.savetxt("labels", labels)
 np.savetxt("ytest", y_test)
 np.savetxt("prob", prob)
+
+max_p = np.amax(prob, axis=1)
+
+tmp = np.stack((x_test_id, labels, max_p)).T
+
+print tmp.shape
+
+np.savetxt("output", tmp)
 
 print clf.oob_score_
 
---
+++ tmp_testing/write.py
@@ -0,0 +1,20 @@
+# with open("diag_grp_1M.csv") as f:
+#     title = f.readline()
+#     with open("output.csv", "w+") as w:
+#         w.write(title)
+#         for lines in f.readlines():
+#             w.write(lines.)
+#
+#
+#
+#
+#
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([3, 4, 5])
+c = np.array([5, 6, 7])
+
+print np.stack((a, b, c)).T
+
+
