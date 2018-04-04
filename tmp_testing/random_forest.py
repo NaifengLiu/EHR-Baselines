9fa6e9ae3c2310ca99b611a4e48ce6f16711a74e
Godot <naifengliu.us@gmail.com>
2018-04-04 00:23:48 -0400

    change location

--- tmp_testing/random_forest.py
+++ tmp_testing/random_forest.py
@@ -1,5 +1,7 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
 import numpy as np
 
 X = np.loadtxt("result")
@@ -7,7 +9,7 @@ y = np.loadtxt("y")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-clf = RandomForestClassifier(n_estimators=10, max_depth=5)
+clf = RandomForestClassifier(n_estimators=10, max_depth=5, oob_score=True)
 
 clf.fit(X_train, y_train)
 labels = clf.predict(X_test)
@@ -16,3 +18,26 @@ prob = clf.predict_proba(X_test)
 np.savetxt("labels", labels)
 np.savetxt("ytest", y_test)
 np.savetxt("prob", prob)
+
+
+Y_test = y_test
+y_score = clf.oob_decision_function_(X_test)
+
+print clf.oob_score
+
+# For each class
+precision = dict()
+recall = dict()
+average_precision = dict()
+for i in range(3):
+    precision[i], recall[i], _ = precision_recall_curve(Y_test[:, i],
+                                                        y_score[:, i])
+    average_precision[i] = average_precision_score(Y_test[:, i], y_score[:, i])
+
+# A "micro-average": quantifying score on all classes jointly
+precision["micro"], recall["micro"], _ = precision_recall_curve(Y_test.ravel(),
+    y_score.ravel())
+average_precision["micro"] = average_precision_score(Y_test, y_score,
+                                                     average="micro")
+print('Average precision score, micro-averaged over all classes: {0:0.2f}'
+      .format(average_precision["micro"]))
