d5c527360384c224738883fcc8ee5b72198de76a
Godot <naifengliu.us@gmail.com>
2018-04-08 19:11:05 -0400

    change location

--- tmp_testing/make_y.py
+++ tmp_testing/make_y.py
@@ -3,18 +3,19 @@ result_group = ['RLS\n', 'PARK\n', 'FIBRO\n']
 
 y = []
 
-with open("./data/training/training.csv") as f:
-    lines = f.readlines()
-    for line in lines:
-        split = line.split(",")
-        if split[-1] not in result_group:
-            print line
-        y.append(result_group.index(split[-1]))
+with open("./data/training/training.csv", "w+") as w:
+    for i in range(1, 8):
+        with open("./data/training/"+str(i)+".csv") as f:
+            f.readline()
+            for line in f.readlines():
+                split = line.split(",")
+                y.append(result_group.index(split[-1]))
 
 y = np.array(y)
 
 print y.shape
 
 np.savetxt("./data/training/y", y)
+
 
 
