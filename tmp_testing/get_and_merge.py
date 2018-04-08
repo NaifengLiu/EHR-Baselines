f14efa0dff0de0d61916280653b8574ea0d5bf8c
Godot <naifengliu.us@gmail.com>
2018-04-08 19:33:16 -0400

    change location

--- tmp_testing/get_and_merge.py
+++ tmp_testing/get_and_merge.py
@@ -20,8 +20,8 @@ import csv
 q = 0
 
 # with open("./data/unknowndata/unknowndata.csv", "w+") as w:
-for i in range(1, 6):
-    with open("./data/unknowndata/"+str(i)+".csv", "rb") as f:
+for i in range(1, 4):
+    with open("./data/testing/"+str(i)+".csv", "rb") as f:
         f.readline()
         reader = csv.reader(f)
         for line in reader:
