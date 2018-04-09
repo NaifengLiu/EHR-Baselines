4c9ed9da7c6cf25aec14482ef19554073fc46de8
Godot <naifengliu.us@gmail.com>
2018-04-08 20:46:25 -0400

    change location

--- tmp_testing/cal_result.py
+++ tmp_testing/cal_result.py
@@ -16,25 +16,25 @@ cell_22 = 0
 
 for i in range(len(y)):
     if int(y[i]) == 0:
-        if int(test_labels) == 0:
+        if int(test_labels[i]) == 0:
             cell_00 += 1
-        elif int(test_labels) == 1:
+        elif int(test_labels[i]) == 1:
             cell_01 += 1
-        elif int(test_labels) == 2:
+        elif int(test_labels[i]) == 2:
             cell_02 += 1
     elif int(y[i]) == 1:
-        if int(test_labels) == 0:
+        if int(test_labels[i]) == 0:
             cell_10 += 1
-        elif int(test_labels) == 1:
+        elif int(test_labels[i]) == 1:
             cell_11 += 1
-        elif int(test_labels) == 2:
+        elif int(test_labels[i]) == 2:
             cell_12 += 1
     elif int(y[i]) == 2:
-        if int(test_labels) == 0:
+        if int(test_labels[i]) == 0:
             cell_20 += 1
-        elif int(test_labels) == 1:
+        elif int(test_labels[i]) == 1:
             cell_21 += 1
-        elif int(test_labels) == 2:
+        elif int(test_labels[i]) == 2:
             cell_22 += 1
 
 print cell_00
