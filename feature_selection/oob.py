bb2fec864cd01b2b7f7077f7febfc1e8c768d2c9
Godot <naifengliu.us@gmail.com>
2018-04-18 10:08:37 -0400

    change location

---
+++ feature_selection/oob.py
@@ -0,0 +1,61 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+from collections import OrderedDict
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+
+# Author: Kian Ho <hui.kian.ho@gmail.com>
+#         Gilles Louppe <g.louppe@gmail.com>
+#         Andreas Mueller <amueller@ais.uni-bonn.de>
+#
+# License: BSD 3 Clause
+
+print(__doc__)
+
+RANDOM_STATE = 123
+
+# Generate a binary classification dataset.
+X = np.loadtxt("./data/X")
+y = np.loadtxt("./data/y")
+
+# NOTE: Setting the `warm_start` construction parameter to `True` disables
+# support for parallelized ensembles but is necessary for tracking the OOB
+# error trajectory during training.
+ensemble_clfs = [
+    ("RandomForestClassifier, max_features=None",
+        RandomForestClassifier(warm_start=True, max_features=None,
+                               oob_score=True,
+                               random_state=RANDOM_STATE))
+]
+
+# Map a classifier name to a list of (<n_estimators>, <error rate>) pairs.
+error_rate = OrderedDict((label, []) for label, _ in ensemble_clfs)
+
+# Range of `n_estimators` values to explore.
+min_estimators = 1
+max_estimators = 100
+
+for label, clf in ensemble_clfs:
+    for i in range(min_estimators, max_estimators + 1):
+        clf.set_params(n_estimators=i)
+        clf.fit(X, y)
+
+        # Record the OOB error for each `n_estimators=i` setting.
+        oob_error = 1 - clf.oob_score_
+        error_rate[label].append((i, oob_error))
+
+fig = plt.figure()
+# Generate the "OOB error rate" vs. "n_estimators" plot.
+for label, clf_err in error_rate.items():
+    xs, ys = zip(*clf_err)
+    plt.plot(xs, ys, label=label)
+
+plt.xlim(min_estimators, max_estimators)
+plt.xlabel("n_estimators")
+plt.ylabel("OOB error rate")
+plt.legend(loc="upper right")
+fig.savefig('temp.png')
