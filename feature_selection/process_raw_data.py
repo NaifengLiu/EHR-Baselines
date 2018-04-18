8fb47a2e1b26aaa3883a9ffada3127d87b75f2c4
Godot <naifengliu.us@gmail.com>
2018-04-18 09:37:56 -0400

    change location

--- feature_selection/process_raw_data.py
+++ feature_selection/process_raw_data.py
@@ -3,18 +3,22 @@ import csv
 
 a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+group_2 = []
+group_4 = []
+group_10 = []
+
 with open("./data/diag_grp_1M_10.csv", "rb") as f:
     f.readline()
     reader = csv.reader(f)
     for line in reader:
         split = line
-        for i in range(len(split)):
-            # print split[i]
-            # print split[i][0]
-            if split[i] == "":
-                continue
-            else:
-                if str(split[i][0]).isalpha():
-                    a[i] = 1
+        if split[2] not in group_2:
+            group_2.append(split[2])
+        if split[4] not in group_4:
+            group_4.append(split[4])
+        if split[10] not in group_10:
+            group_10.append(split[10])
 
-print a
+print group_2
+print group_4
+print group_10
