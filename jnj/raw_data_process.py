9ce28dbc95011c9878a9890e7f5be28adfa5d795
Godot <naifengliu.us@gmail.com>
2018-05-06 19:48:50 -0400

    new method

--- jnj/raw_data_process.py
+++ jnj/raw_data_process.py
@@ -4,14 +4,14 @@ train_data_len = 5
 
 def merge_data():
     with open("./data/test.csv", "w") as w:
-        for i in range(0, test_data_len+1):
+        for i in range(1, test_data_len+1):
             with open("./data/test"+str(i)+".csv") as f:
                 for line in f.readlines():
                     w.write(line)
                 f.close()
 
     with open("./data/train.csv", "w") as w:
-        for i in range(0, train_data_len+1):
+        for i in range(1, train_data_len+1):
             with open("./data/train"+str(i)+".csv") as f:
                 for line in f.readlines():
                     w.write(line)
