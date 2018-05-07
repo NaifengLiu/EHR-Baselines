97b40d861cead3c04d424155640048b526778051
Godot <naifengliu.us@gmail.com>
2018-05-06 21:11:04 -0400

    jnj

--- jnj/cal_result.py
+++ jnj/cal_result.py
@@ -1,19 +1,16 @@
 import numpy as np
 
+# ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'PAD_ACS', 'CHF']
+
 y_result = np.loadtxt("./result/y_result")
 y_truth = np.loadtxt("./data/y_test")
 
 right = 0
 wrong = 0
 
-for i in range(len(y_result)):
-    if y_result[i] == y_truth[i]:
-        right += 1
-    else:
-        wrong += 1
+matrix = np.array((9, 9))
 
-print right
-print wrong
+for i in range(len(y_truth)):
+    matrix[y_truth[i]][y_result[i]] += 1
 
-
-
+print matrix
