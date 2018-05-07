e39f86e1474c5e18dfd630d05422d3141309c5f8
Godot <naifengliu.us@gmail.com>
2018-05-06 20:51:53 -0400

    jnj

---
+++ jnj/cal_result.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+y_result = np.loadtxt("./result/y_result")
+y_truth = np.loadtxt("./data/y_test")
+
+right = 0
+wrong = 0
+
+for i in range(len(y_result)):
+    if y_result == y_truth:
+        right += 1
+    else:
+        wrong += 1
+
+print right
+print wrong
+
+
+
