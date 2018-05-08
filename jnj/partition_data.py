d5c5eab4f5e289119475e5ee054bc118a29de43f
Godot <naifengliu.us@gmail.com>
2018-05-08 06:42:23 -0400

    jnj

--- jnj/partition_data.py
+++ jnj/partition_data.py
@@ -1,4 +1,7 @@
-classes = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'PAD_ACS', 'CHF']
+classes_40800 = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'CHF']
+classes_3400 = ['PAD_ACS']
+
+# 40800, 3400
 
 # for this_class in classes:
 #     with open("./data/subdata/"+this_class, "w+") as w:
@@ -7,13 +10,24 @@ classes = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 
 #             for line in f.readlines():
 #                 class_name = line.rstrip().split(",")[-1]
 #                 if class_name == this_class:
-#                     if num < 42000:
+#                     if num < 3400:
 #                         w.write(line)
 #                         num += 1
 #             f.close()
 #         w.close()
 
-for this_class in classes:
-    with open("./data/subdata/"+this_class) as f:
-        print len(f.readlines())
-
+for i in range(12):
+    with open("data/5_fold_data/undersampling_data/" + str(i), "w") as w:
+        for this_class in classes_40800:
+            with open("./data/subdata/" + this_class) as f:
+                lines = f.readlines()
+                for line in lines[i * 3400:(i + 1) * 3400]:
+                    w.write(line)
+                f.close()
+        for this_class in classes_3400:
+            with open("./data/subdata/" + this_class) as f:
+                lines = f.readlines()
+                for line in lines:
+                    w.write(line)
+                f.close()
+        w.close()
