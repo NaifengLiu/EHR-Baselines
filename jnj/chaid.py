f270192d7a277aa3389907fd82e9e56ea8a13dfc
Godot <naifengliu.us@gmail.com>
2018-05-08 10:19:00 -0400

    jnj

--- jnj/chaid.py
+++ jnj/chaid.py
@@ -1,12 +1,11 @@
 import numpy as np
 
-for i in range(5):
-    tmp_0 = np.loadtxt("./result/5_fold_result/sum/0")
-    tmp_1 = np.loadtxt("./result/5_fold_result/sum/1")
-    tmp_2 = np.loadtxt("./result/5_fold_result/sum/2")
-    tmp_3 = np.loadtxt("./result/5_fold_result/sum/3")
-    tmp_4 = np.loadtxt("./result/5_fold_result/sum/4")
-    tmp = (tmp_0 + tmp_1 + tmp_2 + tmp_3 + tmp_4) / 5
+tmp_0 = np.loadtxt("./result/5_fold_result/sum/0")
+tmp_1 = np.loadtxt("./result/5_fold_result/sum/1")
+tmp_2 = np.loadtxt("./result/5_fold_result/sum/2")
+tmp_3 = np.loadtxt("./result/5_fold_result/sum/3")
+tmp_4 = np.loadtxt("./result/5_fold_result/sum/4")
+tmp = (tmp_0 + tmp_1 + tmp_2 + tmp_3 + tmp_4) / 5
 
 row = np.sum(tmp, axis=0)
 
