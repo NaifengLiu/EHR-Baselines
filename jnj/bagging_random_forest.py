e1e94f4fda25ec399991156631c484a38bcab417
Godot <naifengliu.us@gmail.com>
2018-05-08 06:22:13 -0400

    jnj

--- jnj/bagging_random_forest.py
+++ jnj/bagging_random_forest.py
@@ -1,5 +1,5 @@
-classes = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'CHF']
-# classes = ['PAD_ACS']
+# classes = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'CHF']
+classes = ['PAD_ACS']
 
 for this_class in classes:
     with open("./data/subdata/"+this_class, "w+") as w:
@@ -8,7 +8,7 @@ for this_class in classes:
             for line in f.readlines():
                 class_name = line.rstrip().split(",")[-1]
                 if class_name == this_class:
-                    if num < 40800:
+                    if num < 3400:
                         w.write(line)
                         num += 1
             f.close()
