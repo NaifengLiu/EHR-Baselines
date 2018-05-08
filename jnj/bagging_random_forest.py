4f285a53bfb47d57d94c6cf0d93d1db0140ad1ad
Godot <naifengliu.us@gmail.com>
2018-05-08 06:21:37 -0400

    jnj

--- jnj/bagging_random_forest.py
+++ jnj/bagging_random_forest.py
@@ -1,15 +1,16 @@
-classes = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'PAD_ACS', 'CHF']
+classes = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'CHF']
+# classes = ['PAD_ACS']
 
-# for this_class in classes:
-#     with open("./data/subdata/"+this_class, "w+") as w:
-#         num = 0
-#         with open("./data/train.csv") as f:
-#             for line in f.readlines():
-#                 class_name = line.rstrip().split(",")[-1]
-#                 if class_name == this_class:
-#                     if num < 42000:
-#                         w.write(line)
-#                         num += 1
-#             f.close()
-#         w.close()
+for this_class in classes:
+    with open("./data/subdata/"+this_class, "w+") as w:
+        num = 0
+        with open("./data/train.csv") as f:
+            for line in f.readlines():
+                class_name = line.rstrip().split(",")[-1]
+                if class_name == this_class:
+                    if num < 40800:
+                        w.write(line)
+                        num += 1
+            f.close()
+        w.close()
 
