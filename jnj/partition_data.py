f761b4fa24d731f8daa2944510a1f20e2bda63cc
Godot <naifengliu.us@gmail.com>
2018-05-08 07:08:35 -0400

    jnj

--- jnj/partition_data.py
+++ jnj/partition_data.py
@@ -1,5 +1,5 @@
-classes_40800 = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'CHF']
-classes_3400 = ['PAD_ACS']
+# classes_40800 = ['ISCHEMIC_STROKE', 'CAD_ONLY', 'CAD_ACS', 'ACS', 'AFIB', 'PAD_ONLY', 'ALL_OTHER', 'CHF']
+# classes_3400 = ['PAD_ACS']
 
 # 40800, 3400
 
@@ -32,7 +32,30 @@ classes_3400 = ['PAD_ACS']
 #                 f.close()
 #         w.close()
 
-for i in range(12):
-    with open("data/5_fold_data/undersampling_data/" + str(i)) as f:
-        print len(f.readlines())
+# for i in range(12):
+#     with open("data/5_fold_data/undersampling_data/" + str(i)) as f:
+#         print len(f.readlines())
+
+a = range(3400)
+folds_index = []
+for i in range(5):
+    b = range(i*680, (i+1)*680)
+    c = [item for item in a if item not in b]
+    folds_index.append(c)
+
+print folds_index
+
+for i in range(5):
+    for j in range(12):
+        with open("./data/5_fold_data/" + str(i) + "_" + str(j), "w") as w:
+            with open("./data/5_fold_data/undersampling_data/" + str(j)) as f:
+                lines = f.readlines()
+                for k in range(9):
+                    this_class = lines[k*3400:(k+1)*3400]
+                    tmp = this_class[folds_index[i]]
+                    for line in tmp:
+                        w.write(line)
+                f.close()
+            w.close()
+
 
