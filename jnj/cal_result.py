acad53d61c8e0bfd7560c610f9a6ec56afbb663f
Godot <naifengliu.us@gmail.com>
2018-05-08 08:52:31 -0400

    jnj

--- jnj/cal_result.py
+++ jnj/cal_result.py
@@ -52,16 +52,21 @@ import numpy as np
 #     np.savetxt("./result/5_fold_result/"+str(i), tmp)
 
 
-for i in range(5):
-    tmp_0 = np.loadtxt("./result/5_fold_result/sum/0")
-    tmp_1 = np.loadtxt("./result/5_fold_result/sum/1")
-    tmp_2 = np.loadtxt("./result/5_fold_result/sum/2")
-    tmp_3 = np.loadtxt("./result/5_fold_result/sum/3")
-    tmp_4 = np.loadtxt("./result/5_fold_result/sum/4")
-    tmp = (tmp_0 + tmp_1 + tmp_2 + tmp_3 + tmp_4) / 5
-    np.savetxt("./result/5_fold_result/sum/sum", tmp.argmax(axis=1))
+# for i in range(5):
+#     tmp_0 = np.loadtxt("./result/5_fold_result/sum/0")
+#     tmp_1 = np.loadtxt("./result/5_fold_result/sum/1")
+#     tmp_2 = np.loadtxt("./result/5_fold_result/sum/2")
+#     tmp_3 = np.loadtxt("./result/5_fold_result/sum/3")
+#     tmp_4 = np.loadtxt("./result/5_fold_result/sum/4")
+#     tmp = (tmp_0 + tmp_1 + tmp_2 + tmp_3 + tmp_4) / 5
+#     np.savetxt("./result/5_fold_result/sum/sum", tmp.argmax(axis=1))
 
+y_result = np.loadtxt("./result/5_fold_result/sum/sum")
+y_truth = np.loadtxt("./data/y_test")
 
+right = 0
+wrong = 0
 
-
-
+print right
+print wrong
+print float(right) / float(right+wrong)
